717895232afa2955da8932a7bad8c8a8c0039dc6
Peter Kimani <peterjajakim@gmail.com>
2022-06-17 12:57:09 +0300

    LAB COMPLETE

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // move through the word from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) 
+  {
+    // check each letter to the correponding letter from the end of the name
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+If the first letter is the same as the last letter and the second letter is equal to the second last letter and so on, hence return true , otherwise, return false
+
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
